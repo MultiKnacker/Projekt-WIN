b3843db20863daff99ccd182d3788e23d426fead
FabianShalk <fabian.kalk@comrize.de>
2024-06-18 20:54:30 +0200

    - work on remove function - added add Location function

--- flask/blueprints/locations.py
+++ flask/blueprints/locations.py
@@ -44,16 +44,9 @@ def filter_location():
   show_navbar = True
   return render_template("location/locationview.html", show_navbar = show_navbar, data=filtered_locations)
 
-@location_bp.route('/edit/<location_id>', methods=['GET', 'POST'])
+@location_bp.route('/locations/edit/<location_id>', methods=['GET', 'POST'])
 def edit_location(location_id):
-  selected_location = None
-  if location_id == None:
-    flash('Please select a location.', 'error')
-
-  if request.method == 'GET':
-    # Pre-populate form fields based on selected_location data
-    return render_template('edit_location_modal.html', location=selected_location)
-  elif request.method == 'POST':
+  if request.method == 'POST':
       filter_criteria = {'_id': ObjectId(location_id)}
       retrived_location = location_template(request.form.get('name'),
                                             request.form.get('zipcode'),
@@ -61,7 +54,6 @@ def edit_location(location_id):
                                             request.form.get('streetname'),
                                             request.form.get('location'),
                                             request.form.get('rent'))
-
       update_criteria = {
           "$set": {
               "name": retrived_location.name,
@@ -72,7 +64,6 @@ def edit_location(location_id):
               "rent": retrived_location.rent
           }
       }
-
       update_response = locations_collection.update_one(filter_criteria, update_criteria)
       show_navbar = True
       try:
@@ -84,7 +75,7 @@ def edit_location(location_id):
                   alert_type = "warning"
                   alert_String = "No changes detected in location data."
           else:
-              alert_type = "warning"
+              alert_type = "error"
               alert_String = "Location with ID " + location_id + " not found."
       except pymongo.errors.PyMongoError as e:
           alert_type = "error"
@@ -96,3 +87,68 @@ def edit_location(location_id):
                              data=updated_documents,
                              alert_type=alert_type,
                              alert_String=alert_String)
+
+@location_bp.route('/locations/add', methods=['GET', 'POST'])
+def add_location():
+    if request.method == 'POST':
+        location_data = {
+            "name": request.form.get('name'),
+            "zipcode": request.form.get('zipcode'),
+            "region": request.form.get('region'),
+            "streetname": request.form.get('streetname'),
+            "location": request.form.get('location'),
+            "rent": request.form.get('rent')
+        }
+
+        added_response = locations_collection.insert_one(location_data)
+        show_navbar = True
+        try:
+            if added_response.acknowledged == True:
+                alert_type = "success"
+                alert_String = "Location added successfully!"
+            else:
+                alert_type = "error"
+                alert_String = "Couldnt add the location. Please try again."
+        except pymongo.errors.PyMongoError as e:
+            alert_type = "error"
+            alert_String = "An error occurred while adding the location: " + e
+
+        updated_documents = locations_collection.find({}, projection={"employees": 0, "vehicles": 0}).sort("name")
+        return render_template("location/locationview.html",
+                           show_navbar=show_navbar,
+                           data=updated_documents,
+                           alert_type=alert_type,
+                           alert_String=alert_String)
+
+@location_bp.route('/locations/delete', methods=['GET', 'POST'])
+def delete_locations():
+    selected_locations = request.form.getlist("selected_locations[]")
+    if selected_locations:
+        delte_response = locations_collection.delete_many(selected_locations)
+        show_navbar = True
+        try:
+            if delte_response.acknowledged == True:
+                alert_type = "success"
+                alert_String = "Location added successfully!"
+            else:
+                alert_type = "warning"
+                alert_String = "Couldnt add the location. Please try again."
+        except pymongo.errors.PyMongoError as e:
+            alert_type = "error"
+            alert_String = "An error occurred while adding the location: " + e
+
+        updated_documents = locations_collection.find({}, projection={"employees": 0, "vehicles": 0}).sort("name")
+        return render_template("location/locationview.html",
+                               show_navbar=show_navbar,
+                               data=updated_documents,
+                               alert_type=alert_type,
+                               alert_String=alert_String)
+    else:
+        updated_documents = locations_collection.find({}, projection={"employees": 0, "vehicles": 0}).sort("name")
+        alert_type = "error"
+        alert_String = "Couldnt add the location. Please try again."
+        return render_template("location/locationview.html",
+                               show_navbar=True,
+                               data=updated_documents,
+                               alert_type=alert_type,
+                               alert_String=alert_String)
