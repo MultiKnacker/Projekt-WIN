0f32310be5048b933f3e3d1f1baf3845513374be
FabianShalk <fabian.kalk@comrize.de>
2024-06-21 16:08:51 +0200

    added weasyprint demo on the performanceview

--- flask/blueprints/performance.py
+++ flask/blueprints/performance.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, abort, flash, session, redirect, url_for
+from flask import Blueprint, render_template, abort, flash, session, redirect, url_for, Response
+from weasyprint import HTML
 
 performance_bp = Blueprint("performance", __name__, template_folder='templates')
 
@@ -8,5 +9,16 @@ def list_performance():
     flash('Please log in to access this page.', 'error')
     return redirect(url_for('login.login'))
 
-  show_navbar = True
-  return render_template("performanceview.html", show_navbar=show_navbar)
+  # Render HTML content
+  html_content = render_template("performanceview.html")
+
+  # Generate PDF using WeasyPrint
+  pdf = HTML(string=html_content).write_pdf()
+
+  # Optionally, you can save the PDF to a file
+  # with open('output.pdf', 'wb') as f:
+  #     f.write(pdf)
+
+  # Return PDF as a response
+  return Response(pdf, mimetype='application/pdf')
+
